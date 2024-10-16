5ec241c7aa54589b6a1d29261f7bd3bd26baf601
Ridwan <wemzain@gmail.com>
2024-10-16 16:15:46 +0100

    Splitted the product router into routes and controllers

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,18 +1,14 @@
 import { Router } from "express";
+import { listProducts, getProductById, createProduct, updateProduct, deleteProduct } from "./productController";
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send('list of products.....')
-})
+router.get('/',listProducts)
 
-router.get('/:id', (req,res) => {
-    console.log(req.params)
-    res.send('a product')
-})
+router.get('/:id', getProductById)
 
-router.post('/', (req, res) => {
-    res.send('New product created')
-})
+router.post('/', createProduct)
+router.put('/:id', updateProduct)
+router.delete('/:id', deleteProduct)
 
 export default router
---
+++ api/src/routes/products/productController.ts
@@ -0,0 +1,17 @@
+import {Request, Response} from 'express'
+
+export function listProducts (req: Request, res: Response)  {
+    res.send('list of products.....')
+}
+export function getProductById (req: Request, res: Response)  {
+    res.send('Product')
+}
+export function createProduct (req: Request, res: Response)  {
+    res.send('New Product added')
+        }
+export function updateProduct (req: Request, res: Response)  {
+    res.send('Product updated...')
+}
+export function deleteProduct (req: Request, res: Response)  {
+    res.send('Delete Product')
+}
