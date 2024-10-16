f3a6811a597c3d98511604aaa0684022bc6ab18f
Ridwan <wemzain@gmail.com>
2024-10-16 15:47:43 +0100

    Product route boilerplate added

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import productRouter from './routes/products'
 
 const PORT = 8080
 const app = express()
@@ -6,6 +7,8 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
+
+app.use('/products', productRouter)
 
 const tester = "New"
 
